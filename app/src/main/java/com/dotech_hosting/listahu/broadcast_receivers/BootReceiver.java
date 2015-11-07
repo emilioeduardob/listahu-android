d81f1be8fb501ec967c17edfcc8ccdb61aab02a3
Emilio Blanco <emilioeduardob@gmail.com>
2015-11-07 14:54:40 -0300

    fix(): Restart monitoring after App upgrade

--- app/src/main/java/com/dotech_hosting/listahu/broadcast_receivers/BootReceiver.java
+++ app/src/main/java/com/dotech_hosting/listahu/broadcast_receivers/BootReceiver.java
@@ -3,6 +3,7 @@ package com.dotech_hosting.listahu.broadcast_receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.dotech_hosting.listahu.broadcast_receivers.AlarmReceiver;
 import com.dotech_hosting.listahu.services.CallDetectService;
@@ -11,8 +12,11 @@ import com.dotech_hosting.listahu.services.CallDetectService;
  * Created by emilio on 10/29/15.
  */
 public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = BootReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Starting services");
         Intent i = new Intent(context, CallDetectService.class);
         context.startService(i);
 
