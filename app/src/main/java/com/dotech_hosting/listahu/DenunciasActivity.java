5ae1216118a5849e9aa3562f2b42ae94539fc865
Emilio Blanco <emilioeduardob@gmail.com>
2015-11-12 07:52:10 -0300

    feat(Denuncias): Adds search action

--- app/src/main/java/com/dotech_hosting/listahu/DenunciasActivity.java
+++ app/src/main/java/com/dotech_hosting/listahu/DenunciasActivity.java
@@ -1,11 +1,18 @@
 package com.dotech_hosting.listahu;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.SearchViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.dotech_hosting.listahu.adapters.DenunciaAdapter;
 import com.dotech_hosting.listahu.models.Denuncia;
@@ -36,6 +43,29 @@ public class DenunciasActivity extends AppCompatActivity {
     private Realm realm;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.denuncias_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setInputType(InputType.TYPE_CLASS_NUMBER);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                queryNumber(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                queryNumber(query);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncias);
@@ -54,7 +84,7 @@ public class DenunciasActivity extends AppCompatActivity {
     private void queryNumber(String number) {
         RealmQuery<Denuncia> query = realm.where(Denuncia.class);
         if (number != null) {
-            query = query.equalTo("numero", number);
+            query = query.beginsWith("numero", number);
         }
         RealmResults<Denuncia> data = query.findAll();
         mAdapter.setData(data);
